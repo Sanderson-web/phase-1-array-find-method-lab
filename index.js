b76b1c86af5251975143c159bcaf6c6a59b8a20e
Sanderson Srisouraj <ssanderson295@gmail.com>
2022-03-31 20:47:00 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+function superbowlWin(record) {
+    function winningYear(year) {
+        if (year.result === "W") {
+            return true
+        } else {
+            return false
+        }
+    }
+    const winningYearObject = record.find(winningYear)
+    if (winningYearObject) {
+        return winningYearObject.year
+    }
+}
